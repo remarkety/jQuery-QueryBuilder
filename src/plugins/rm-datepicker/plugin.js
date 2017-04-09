b4334c15f6ea63c01ec88b0da4bd62dacfea6b35
bnayal <bnaya@remarkety.com>
2017-04-09 17:50:31 +0300

    fix datetimepicker change event

--- src/plugins/rm-datepicker/plugin.js
+++ src/plugins/rm-datepicker/plugin.js
@@ -15,34 +15,32 @@ QueryBuilder.define('rm-datepicker', function(options) {
         Utils.error('MissingLibrary', 'datetimepicker is required to use "rm-datepicker" plugin.');
     }
 
-    function onChange(rule, date){
-        rule.value = date.format('YYYY-MM-DD');
-    }
-
     this.on('afterCreateRuleInput', function(e, rule) {
-        if(rule.filter.type == "date" || rule.filter.type == "datetime") {
+        if(rule.filter.type === "date" || rule.filter.type === "datetime") {
             if(!rule.operator.input_type || rule.operator.input_type !== "integer"){
                 var $inputs = rule.$el.find(Selectors.value_container).find('input');
                 $inputs.datetimepicker(options);
                 $inputs.on('dp.change', function(e){
-                    onChange(rule, e.date);
+                    $(this).trigger('change');
                 });
             }
         }
     });
 
     this.on('afterUpdateRuleOperator', function(e, rule) {
-        if(rule.filter.type == "date" || rule.filter.type == "datetime") {
+        if(rule.filter.type === "date" || rule.filter.type === "datetime") {
             var $inputs = rule.$el.find(Selectors.value_container).find('input');
+            console.log($inputs);
+            $inputs.val('');
             if(!rule.operator.input_type || rule.operator.input_type !== "integer"){
                 $inputs.datetimepicker(options);
-                $inputs.off('dp.change').on('dp.change', function(e){
-                    onChange(rule, e.date);
+                $inputs.on('dp.change', function(e){
+                    $(this).trigger('change');
                 });
             } else {
 
                 $inputs.each(function(){
-                    if($(this).data("DateTimePicker") && typeof $(this).data("DateTimePicker").destroy == "function"){
+                    if($(this).data("DateTimePicker") && typeof $(this).data("DateTimePicker").destroy === "function"){
                         $(this).data("DateTimePicker").destroy();
                         $(this).val('');
                     };
